3d999aadca69454725a25e8b51bdc381cd42bcf0
khyanaprabhadas <khyanaprabha.das@yahoo.com>
2022-08-30 13:06:51 +0530

    rest api documentation

---
+++ springboot-swagger-test/src/main/java/com/javainuse/swaggertest/SpringbootSwaggerTestApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.swaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSwaggerTestApplication.class, args);
+	}
+
+}
